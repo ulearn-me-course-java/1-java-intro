73f755f532bcb2fbdd7cbd548f044291067aa664
wisedog706 <psnetwork1138@gmail.com>
2019-02-09 18:36:27 +0500

    HELLO WORLD!!!

--- task01/src/HelloWorld.java
+++ task01/src/HelloWorld.java
@@ -1,3 +1,3 @@
 public class HelloWorld {
-    public static void main(String args[]) { System.out.println("Hello, World!"); }
+    public static void main(String values[]) { System.out.println("Hello, World!"); }
 }
