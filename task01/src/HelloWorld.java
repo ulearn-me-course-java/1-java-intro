d341084b681f3a310050c20737a19b04757691bf
пк <nikitos.wie@mail.ru>
2021-02-18 16:57:17 +0300

    Яхин Никита. Задание - 1

--- task01/src/HelloWorld.java
+++ task01/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void main() {
-        System.out.println("Hello_World!");
+    public static void main(String args[]) {
+        System.out.println("Hello, World!");
     }
 }
